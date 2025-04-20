625c6098dc218c0ee674a8bdced108009190e976
Yves <ybyiring@gmail.com>
2025-04-20 10:15:30 +0200

    updated configs

--- backend/truck_driver_hos/settings.py
+++ backend/truck_driver_hos/settings.py
@@ -70,10 +70,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),           
-        'PASSWORD': 'TrDLoB03!25',   
-        'HOST': 'localhost',            
-        'PORT': '5432',  
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
