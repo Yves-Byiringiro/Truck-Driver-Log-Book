d5a4fead363765619e5c31afa7a2b0f3c2ac0b95
Yves <ybyiring@gmail.com>
2025-04-20 13:03:49 +0200

    Added salutation section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,11 @@ export default function Home() {
   return (
     <div>
       <MainContainer>
-        <div>
+        <div className='flex flex-col gap-10'>
             <PageInfoSection msg="Are you ready to start your journey? Please fill in the form below." />
+            <div className='bg-white shadow-md rounded-lg p-6'>
+                <h1 className='text-lg font-semibold'>New log book</h1>
+            </div>
         </div>
       </MainContainer>
     </div>
