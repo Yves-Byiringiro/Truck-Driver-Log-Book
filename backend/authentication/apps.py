b0b579b9477f3391f8f3f09a5b18be63fbd4a09f
Yves <ybyiring@gmail.com>
2025-05-06 11:06:14 +0200

    Added comments in core app files

--- backend/authentication/apps.py
+++ backend/authentication/apps.py
@@ -1,5 +1,6 @@
-from django.apps import AppConfig
+#Entrypoint
 
+from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
